c2e18d57793a65aaeb126b38205e8e83a37a5573
EdgardOliveira <50224075+EdgardOliveira@users.noreply.github.com>
2019-11-27 09:02:49 -0400

    inicial

---
+++ ListarArquivos/frmListarArquivos.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace ListaArquivos
+{
+    partial class frmListarArquivos
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnListar = new System.Windows.Forms.Button();
+            this.lblQuantidade = new System.Windows.Forms.Label();
+            this.rchtxtArquivos = new System.Windows.Forms.RichTextBox();
+            this.fbdSelecionarDiretorio = new System.Windows.Forms.FolderBrowserDialog();
+            this.txtbxCaminho = new System.Windows.Forms.TextBox();
+            this.btnSaidaFirewall = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // btnListar
+            // 
+            this.btnListar.Location = new System.Drawing.Point(144, 4);
+            this.btnListar.Margin = new System.Windows.Forms.Padding(2);
+            this.btnListar.Name = "btnListar";
+            this.btnListar.Size = new System.Drawing.Size(58, 20);
+            this.btnListar.TabIndex = 0;
+            this.btnListar.Text = "Listar";
+            this.btnListar.UseVisualStyleBackColor = true;
+            this.btnListar.Click += new System.EventHandler(this.btnListar_Click);
+            // 
+            // lblQuantidade
+            // 
+            this.lblQuantidade.AutoSize = true;
+            this.lblQuantidade.Location = new System.Drawing.Point(141, 365);
+            this.lblQuantidade.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblQuantidade.Name = "lblQuantidade";
+            this.lblQuantidade.Size = new System.Drawing.Size(124, 13);
+            this.lblQuantidade.TabIndex = 2;
+            this.lblQuantidade.Text = "Quantidade de Arquivos:";
+            // 
+            // rchtxtArquivos
+            // 
+            this.rchtxtArquivos.Location = new System.Drawing.Point(144, 53);
+            this.rchtxtArquivos.Margin = new System.Windows.Forms.Padding(2);
+            this.rchtxtArquivos.Name = "rchtxtArquivos";
+            this.rchtxtArquivos.Size = new System.Drawing.Size(228, 310);
+            this.rchtxtArquivos.TabIndex = 3;
+            this.rchtxtArquivos.Text = "";
+            // 
+            // txtbxCaminho
+            // 
+            this.txtbxCaminho.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.txtbxCaminho.Enabled = false;
+            this.txtbxCaminho.Location = new System.Drawing.Point(144, 29);
+            this.txtbxCaminho.Margin = new System.Windows.Forms.Padding(2);
+            this.txtbxCaminho.Name = "txtbxCaminho";
+            this.txtbxCaminho.Size = new System.Drawing.Size(228, 20);
+            this.txtbxCaminho.TabIndex = 4;
+            // 
+            // btnSaidaFirewall
+            // 
+            this.btnSaidaFirewall.Location = new System.Drawing.Point(212, 4);
+            this.btnSaidaFirewall.Margin = new System.Windows.Forms.Padding(2);
+            this.btnSaidaFirewall.Name = "btnSaidaFirewall";
+            this.btnSaidaFirewall.Size = new System.Drawing.Size(160, 20);
+            this.btnSaidaFirewall.TabIndex = 5;
+            this.btnSaidaFirewall.Text = "Bloquear Saída no Firewall";
+            this.btnSaidaFirewall.UseVisualStyleBackColor = true;
+            this.btnSaidaFirewall.Click += new System.EventHandler(this.btnListar_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Black;
+            this.panel1.Location = new System.Drawing.Point(7, 4);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(129, 384);
+            this.panel1.TabIndex = 6;
+            // 
+            // frmListarArquivos
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(382, 387);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.btnSaidaFirewall);
+            this.Controls.Add(this.txtbxCaminho);
+            this.Controls.Add(this.rchtxtArquivos);
+            this.Controls.Add(this.lblQuantidade);
+            this.Controls.Add(this.btnListar);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "frmListarArquivos";
+            this.Text = "Arquivos";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnListar;
+        private System.Windows.Forms.Label lblQuantidade;
+        private System.Windows.Forms.RichTextBox rchtxtArquivos;
+        private System.Windows.Forms.FolderBrowserDialog fbdSelecionarDiretorio;
+        private System.Windows.Forms.TextBox txtbxCaminho;
+        private System.Windows.Forms.Button btnSaidaFirewall;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ ListarArquivos/frmListarArquivos.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Windows.Forms;
+
+namespace ListaArquivos
+{
+    public partial class frmListarArquivos : Form
+    {
+        private bool bloquearFirewall = false;
+
+        public frmListarArquivos()
+        {
+            InitializeComponent();
+        }
+
+        private bool SelecionarDiretorio()
+        {
+            bool status = false;
+
+            if (fbdSelecionarDiretorio.ShowDialog() == DialogResult.OK)
+            {
+                txtbxCaminho.Text = fbdSelecionarDiretorio.SelectedPath;
+                status = true;
+            }
+            else
+            {
+                MessageBox.Show("É necessário selecionar uma pasta");
+            }
+
+            return status;
+        }
+
+        private void ListarArquivos(string tiposArquivos)
+        {
+            //Inicialização da quantidade de arquivos
+            int qtdeArquivos = 0;
+
+            //limpando o conteúdo do RichText
+            rchtxtArquivos.Clear();
+            
+            //configurando o diretório a ser listado
+            DirectoryInfo directoryInfo = new DirectoryInfo(txtbxCaminho.Text);
+
+            //Pesquisando arquivos no diretório informado e armazenando em um array de arquivos
+            FileInfo[] fileInfos = directoryInfo.GetFiles(tiposArquivos);
+
+            //Para cada arquivo encontrado em fileinfos... adicione-o no richtext
+            foreach (FileInfo file in fileInfos)
+            {
+                //Adiciona o arquivo encontrado no Richtext
+                rchtxtArquivos.AppendText(file.Name + Environment.NewLine);
+
+                //verifica se é para bloquear o arquivo no firewall
+                if (bloquearFirewall) {
+                    //bloquear está habilitado, então envia o caminho e o nome do arquivo para adicionar a regra de bloqueio de saída no firewall
+                    BloquearSaidaProgramasFirewall(txtbxCaminho.Text, file.Name);
+                }
+                
+                //Incrementa a quantidade de arquivos encontrados
+                qtdeArquivos++;
+
+                //Configurando a quantidade de arquivos no Label
+                lblQuantidade.Text = "Quantidade de Arquivos: " + qtdeArquivos.ToString();
+            }
+        }
+
+        private void BloquearSaidaProgramasFirewall(string caminho, string arquivo)
+        {
+            string caminhoCompleto = Path.Combine(caminho, arquivo);
+
+            //montando o comando para fazer o bloqueio no firewal avançado do windows 
+            string comando = "netsh advfirewall firewall add rule name=\"" + arquivo + "\" dir=out action=block profile=any protocol=any program=\"" + caminhoCompleto + "\" enable=yes";
+
+            ProcessStartInfo processStartInfo = new ProcessStartInfo
+            {
+                UseShellExecute = true,
+
+                WorkingDirectory = @"C:\Windows\System32",
+
+                FileName = @"C:\Windows\System32\cmd.exe",
+
+                Verb = "runas",
+
+                Arguments = "/c " + comando,
+
+                WindowStyle = ProcessWindowStyle.Hidden
+            };
+
+            Process.Start(processStartInfo);
+        }
+
+        private void SalvarResultado()
+        {
+            //salvando o conteúdo do Richtext (listagem no arquivos) em um arquivo
+            rchtxtArquivos.SaveFile(txtbxCaminho.Text + @"\listagem.txt", RichTextBoxStreamType.PlainText);
+        }
+
+        private void btnListar_Click(object sender, EventArgs e)
+        {
+            Button btn = (Button) sender;
+
+            bool selecionarProgramas = false;
+            bool selecionarResultado = false;
+
+            //verificando qual botão está chamando
+            switch (btn.Name){
+                case "btnListar": 
+                    {
+                        bloquearFirewall = false;
+                        break;
+                    }
+                case "btnSaidaFirewall":
+                    {
+                        bloquearFirewall = true;
+                        break;
+                    }
+            }
+
+            //Selecionar o diretório a ser pesquisado
+            selecionarProgramas = SelecionarDiretorio();
+
+            if (selecionarProgramas)
+            {
+                switch (bloquearFirewall)
+                {
+                    case true :
+                        {
+                            ListarArquivos("*.exe");
+                            break;
+                        }
+                    case false :
+                        {
+                            ListarArquivos("*.*");
+                            MessageBox.Show("Informe o caminho onde deseja salvar o arquivo texto.");
+
+                            //Selecionar o diretório onde será salvo o arquivo texto
+                            selecionarResultado = SelecionarDiretorio();
+
+                            if (selecionarResultado)
+                            {
+                                //Salva a listagem de arquivos em um arquivo texto no local previamente escolhido na etapa anterior
+                                SalvarResultado();
+                            }
+                            break;
+                        }
+                }
+            }
+        }
+    }
+}
